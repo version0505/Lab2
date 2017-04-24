802a61ab679f4f896009c20c99b31f2c15260277
Ju Ho Choi <rl2080vg@mnstate.edu>
2017-04-24 01:36:45 -0500

    created test for createMeny, added doc

---
+++ testMenu.py
@@ -0,0 +1,18 @@
+from createMenu import *
+
+first = ["Add","Delete","Check","Print","Exit"]
+print(createMenu(first))
+
+second = ["Deposit","Withdraw","Print","Exit"]
+print(createMenu(second))
+
+third = ["Send","Check","Delete","Mark","Exit"]
+print(createMenu(third))
+
+fourth = ["Insert","Copy","Paste","Delete","Exit"]
+print(createMenu(fourth))
+
+fifth = ["GCD","LCM","T/F","Subset","Valid","Exit"]
+print(createMenu(fifth))
+
+    
